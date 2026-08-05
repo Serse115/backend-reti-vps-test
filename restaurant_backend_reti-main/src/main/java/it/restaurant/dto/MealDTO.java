a85c72440150a51cba6d43ad0f9e4d2d70323e3c
a.elidrissi <146013182+Serse115@users.noreply.github.com>
2026-08-05 02:22:14 +0200

    try with the fixed query

---
+++ restaurant_backend_reti-main/src/main/java/it/restaurant/dto/MealDTO.java
@@ -0,0 +1,11 @@
+package it.restaurant.dto;
+
+public class MealDTO {
+
+    public String mealCode;
+    public String category;
+    public String name;
+    public double price;
+    public String description;
+    public int availability;
+}
