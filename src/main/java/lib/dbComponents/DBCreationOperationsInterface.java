165e9fe65bbd055495ffb073eefeb4cd0b36c626
a.elidrissi <146013182+Serse115@users.noreply.github.com>
2026-08-25 20:53:56 +0200

    back with the emails endpoints
    fixed the join in the delivery orders issues and back with the emails endpoints

---
+++ src/main/java/lib/dbComponents/DBCreationOperationsInterface.java
@@ -0,0 +1,9 @@
+package lib.dbComponents;
+
+/******** Interface for the CreationOperations ********/
+public interface DBCreationOperationsInterface {
+
+    // Methods
+    // Full new DB creation
+    void fullDBCreation(String user, String password);
+}
