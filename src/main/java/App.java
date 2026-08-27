20cb7bfe46e56c203e82be1991696c2809e2a9af
a.elidrissi <146013182+Serse115@users.noreply.github.com>
2026-08-28 00:38:49 +0200

    new working version
    needs cleaning of some classes but works

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+/* Starting class App */
+public class App {
+    public static void main(String[] args) {
+
+        // ON THE VPS THE RESTAURANT APPLICATION IS THE REAL STARTING POINT
+        // Starting the application
+        //final DBConnectionClassInterface connector = new DBConnectionClass();   // Creating the connector object
+        //new DatabaseConnectionPage(connector);                                  // Passing the connector object to connect to the Database and start the application
+    }
+}
