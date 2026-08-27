5e18a80dd89509b876984f4d932045ae0827e3ea
a.elidrissi <146013182+Serse115@users.noreply.github.com>
2026-08-27 23:03:50 +0200

    Delete src/main directory

--- src/main/java/lib/dbComponents/GetAndDownloadDataInterface.java
+++
@@ -1,9 +0,0 @@
-package lib.dbComponents;
-
-/******** Interface for the GetAndDownloadData class ********/
-public interface GetAndDownloadDataInterface {
-
-    // Methods
-    // Get the orders through the table number method
-    void getOrdersThroughTableNum(String tableNum, String filePath);
-}
